51eb209ab8da1d5667d11f81201563578156b27c
Artur Z <zarzecki.art@icloud.com>
2021-03-11 12:04:41 +0100

    Add one scn to scaneview in popover

--- ramp-up/Controller/RampPickerVC.swift
+++ ramp-up/Controller/RampPickerVC.swift
@@ -29,6 +29,18 @@ class RampPickerVC: UIViewController {
         sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         view.insertSubview(sceneView, at: 0)
         
+        let scene = SCNScene(named: "art.scnassets/ramps.scn")!
+        sceneView.scene = scene
+        
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        scene.rootNode.camera = camera
+        
+        let obj = SCNScene(named: "art.scnassets/pipe.scn")
+        let node = obj?.rootNode.childNode(withName: "pipe", recursively: true)!
+        node?.scale = SCNVector3(0.002, 0.002, 0.002)
+        node?.position = SCNVector3Make(0, 0.7, 0)
+        scene.rootNode.addChildNode(node!)
         preferredContentSize = size
     }
     
