081841d2e473a5b7957a0ce1dc9c5daa935ce2df
Artur Z <zarzecki.art@icloud.com>
2021-03-11 10:51:27 +0100

    Make a secound VC as a PopOver

---
+++ ramp-up/Controller/RampPickerVC.swift
@@ -0,0 +1,39 @@
+//
+//  RampPickerVSViewController.swift
+//  ramp-up
+//
+//  Created by Artur Zarzecki on 11/03/2021.
+//
+
+import UIKit
+import SceneKit
+
+class RampPickerVC: UIViewController {
+    
+    var sceneView: SCNView!
+    var size: CGSize!
+    
+    init(size: CGSize) {
+        super.init(nibName: nil, bundle: nil)
+        self.size = size
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
+        
+        preferredContentSize = size
+    }
+    
+    
+
+    
+
+}
